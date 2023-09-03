bdf04152ff8ceef70de52d926a3491c21c0b7ed0
Mudassar Amin <mudasiramin456@gmail.com>
2023-09-03 09:15:46 +0500

    commit

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-93dyo$$zh=aap67y)7*(7z%0_!r*5+5&en9z-=3!*j7ma6hkkn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iclickevent.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
